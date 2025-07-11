f95875b53d8e3ad1c926439ad8eebcfc3fc5a46d
AbhinayAmbati <abhinayambati4@gmail.com>
2025-07-11 23:21:16 +0530

    Added detailed explanation for setNum and getNum

--- arc-01/SetNumAndGetNum.java
+++ arc-01/SetNumAndGetNum.java
@@ -1,41 +1,58 @@
 
-class SubClass{
+class SubClass //  This is a sub class
+{
 
-    private int n;
-    private String s;
+    private int n; // This is a private variable which can not be accessed other classes directly 
+    private String s; // This is a private variable which can not be accessed other classes directly
 
-    public String getS(){
-        return s;
+    public String getS() // This is a public get method which returns the value of s when the method is called
+    {
+        return s; // returns the value of x
     }
 
-    public int getN(){
-        return n;
+    public int getN() // This is a public get method which returns the value of n when the method is called
+    {
+        return n; // returns the value of n
     }
 
-    public void setN(int n){
+    public void setN(int n) 
+    // This is a set method which sets the value to the private n variable by other classes by passing parameters to the set method
+    {
         this.n = n;
+        // here this.n refers to the current object instance variable and n refers to the parameter passed by the other class 
     }
 
-    public void setS(String s){
+    public void setS(String s)
+    // This is a set method which sets the value to the private s variable by other classes by passing parameters to the set method
+
+    {
         this.s = s;
+        // here this.s refers to the current object instance variable and s refers to the parameter passed by the other class 
+
     }
 
-    public void printSettersValue(){
+    public void printSettersValue() 
+    // This method prints values of priavte variables when the method is called
+    {
         System.out.println("Value of n is : " + n + "\n" + "Value of s is : " + s);
+        // Printing the values of the pprivate variables
     }
 
 } 
 
 
-public class SetNumAndGetNum {
+public class SetNumAndGetNum // Main Class
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) // main method
+    {
         
-        SubClass s = new SubClass();
+        SubClass s = new SubClass(); // Instance of the sub class
 
-        s.setN(1);
-        s.setS("LCJPCXK");
-        s.printSettersValue();
+        s.setN(1); // Passing the parameter of value 1 to the setN method of class SubClass
+        s.setS("LCJPCXK"); // Passing the parameter of value LCJPCXK to the setS method of class SubClass
+        s.printSettersValue(); 
+        // Calling the printSettersValue method to print the values that we passed in the parameters
         
     }
     
