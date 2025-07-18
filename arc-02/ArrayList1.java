3df872f499464140e853fb0b8c64c9da59a935e3
abhinayambati4@gmail.com <abhinayambati4@gmail.com>
2025-07-18 16:15:15 +0530

    Implemented different methods in ArrayList

--- arc-02/ArrayList1.java
+++ arc-02/ArrayList1.java
@@ -1,14 +1,38 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class ArrayList1 {
 
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>();
 
-        arrayList.add(1);
+        Scanner s = new Scanner(System.in);
+
+        for(int i=0;i<5;i++){
+            arrayList.add(s.nextInt());
+        }
 
         System.out.println(arrayList);
+
+        Collections.sort(arrayList);
+
+        System.out.println(arrayList);
+
+        for(int i=0;i<arrayList.size()-1;i++){
+            System.out.print(arrayList.get(i)+ " ");
+        }
+
+        arrayList.add(2, 54);
+        arrayList.remove(2);
+
+        System.out.println(arrayList.get(2));
+
+        ArrayList<Integer> b = (ArrayList<Integer>) arrayList.clone();
+
+        System.out.println(b);
+
 
     }
 }
