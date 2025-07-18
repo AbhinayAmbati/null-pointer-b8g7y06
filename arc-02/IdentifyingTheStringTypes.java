b5ee5c84e8718a81ab23bb0715bc8164078af122
abhinayambati4@gmail.com <abhinayambati4@gmail.com>
2025-07-18 15:00:43 +0530

    Written logic to identify the keywords, identifiers and others given in a input sentence

--- arc-02/IdentifyingTheStringTypes.java
+++ arc-02/IdentifyingTheStringTypes.java
@@ -1,3 +1,58 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+class Identifier{
+    public void letsIdentify(String s){
+         String[] keyWords = {"int", "boolean", "char", "byte", "short", "long", "float", "double"};
+        String[] punctuations = {"{", ";", "}", "[", "]", "'", "."};
+        String[] specialChar = {"=", "+", "-", "*", "&", "%", "/", "<", ">", "!"};
+        String[] numbers = {"0","1","2","3","4","5","6","7","8","9"};
+
+        Set<String> keySet = new HashSet<>(Arrays.asList(keyWords));
+        Set<String> punctSet = new HashSet<>(Arrays.asList(punctuations));
+        Set<String> specSet = new HashSet<>(Arrays.asList(specialChar));
+        Set<String> numSet = new HashSet<>(Arrays.asList(numbers));
+
+        
+
+        String[] tokens = s.split("\\s+");
+
+        for(String token : tokens){
+            if(keySet.contains(token)){
+                System.out.println(token + " - is a keyword");
+            }
+            else if(punctSet.contains(token)){
+                System.out.println(token + " - is a punctuation");
+            }
+            else if(specSet.contains(token)){
+                System.out.println(token + " - is a special character");
+            }
+            else if(numSet.contains(token)){
+                System.out.println(token + " - is a number");
+            }
+            else if(token.matches("[a-zA-Z_][a-zA-Z0-9_]*")){
+                System.out.println(token+" - is a identifier");
+            }
+            else{
+                System.out.println(token + " - is number");
+            }
+        }
+
+    }
+}
+
 public class IdentifyingTheStringTypes {
-    
+    public static void main(String[] args){
+
+        Scanner s = new Scanner(System.in);
+
+        String str = s.nextLine();
+
+        Identifier i = new Identifier();
+
+        i.letsIdentify(str);
+
+    }
 }
--- arc-02/RemovingWhiteSpaces.java
+++ arc-02/RemovingWhiteSpaces.java
@@ -2,17 +2,19 @@ import java.util.*;
 
 class Remover{
     public void whiteSpaceRemover(String s){
+        StringBuilder sb = new StringBuilder();
         int size = s.length();
         for(int i=0;i< size;i++ ){
             if(s.charAt(i) != ' '){
-                System.out.print(s.charAt(i));
+                sb.append(s.charAt(i));
             }
         }
+        System.out.println(sb);
     }
 }
 
 public class RemovingWhiteSpaces{
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
